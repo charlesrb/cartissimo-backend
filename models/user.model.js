2328c9c7ca3dc32ff839b7611a62376551570a60
Charles Reverchon-Billot <charles.reverchon.billot@gmail.com>
2022-10-09 13:50:49 +0200

    Ajout champ password

--- models/user.model.js
+++ models/user.model.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
+  password: { type: String },
   sport: { type: [String] },
   NBA: { type: [String] },
   NFL: { type: [String] },
